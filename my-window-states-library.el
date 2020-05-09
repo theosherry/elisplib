b03697f202439e3a3dd2d81da46ef6e371f48072
theo_sherry <sherr.theo@gmail.com>
2020-05-09 13:40:15 -0400

    Remove winstack functions (using winner mode)

--- my-window-states-library.el
+++ my-window-states-library.el
@@ -18,31 +18,4 @@
      (locate-user-emacs-file "my-libraries/daily_planning.winstate"))
     (window-state-put (read (current-buffer))) (frame-root-window)))
 
-
-(defvar --winstack-stack '()
-  "A Stack holding window configurations.
-Use `my-winstack-push' and
-`my-winstack-pop' to modify it.")
-
-(defun my-winstack-push()
-  "Push the current window configuration onto `--winstack-stack'."
-  (interactive)
-  (if (and (window-configuration-p (car --winstack-stack))
-           (compare-window-configurations (car --winstack-stack)
-					  (current-window-configuration)))
-      (message "Current config already pushed")
-    (progn (push (current-window-configuration) --winstack-stack)
-           (message (concat "pushed "
-			    (number-to-string
-                             (length (window-list (selected-frame))))
-			    " frame config")))))
-
-(defun my-winstack-pop()
-  "Pop the last window configuration off `--winstack-stack' and apply it."
-  (interactive)
-  (if (car --winstack-stack)
-      (progn (set-window-configuration (pop --winstack-stack))
-             (message "popped"))
-    (message "End of window stack")))
-
 (provide 'my-window-states-library)
