c6083db0bf2605f110f26094ccee7fd770be1da1
theo_sherry <sherr.theo@gmail.com>
2020-06-20 11:31:20 -0400

    Update daily planning winstates

--- my-window-states-library.el
+++ my-window-states-library.el
@@ -2,7 +2,7 @@
 
 ;; Use this command to save the current window state to the specfied file
 ;; (with-temp-file
-;;   "~/.emacs.d/my_libraries/daily_planning.winstate"
+;;   (locate-user-emacs-file "my-libraries/daily_planning.winstate")
 ;;   (print (window-state-get (frame-root-window) t) (current-buffer)))
 
 (defun my-start-daily-planning ()
@@ -10,8 +10,9 @@
   (interactive)
   (find-file "~/Dropbox/workspaces/emacs/Organization/projects.org")
   (find-file "~/Dropbox/workspaces/emacs/Organization/someday.org")
-  (find-file "~/Dropbox/workspaces/emacs/Organization/one_offs.org")
-  (find-file "~/Dropbox/workspaces/emacs/Organization/daily_planning.org")
+  (find-file "~/Dropbox/workspaces/emacs/Organization/tickler.org")
+  (find-file "~/Dropbox/workspaces/emacs/Organization/inbox.org")
+  (find-file "~/Dropbox/workspaces/emacs/Organization/reference.org")
   (org-agenda-list)
   (with-temp-buffer
     (insert-file-contents
