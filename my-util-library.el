60c451d7ee357ee2edc1df6618933c6a7717ecde
theo_sherry <sherr.theo@gmail.com>
2020-11-23 19:23:32 -0500

    add function to wrap word or region

--- my-util-library.el
+++ my-util-library.el
@@ -7,6 +7,23 @@
       (let ((region (buffer-substring beginning end)))
 	(delete-region beginning end)
 	region)
-     ""))
+    ""))
+
+(defun my-tag-word-or-region (text-begin text-end)
+  "Surround current word or region with given text. Taken from https://emacs.stackexchange.com/a/3500/26829"
+  (interactive "sStart tag: \nsEnd tag: ")
+  (let (pos1 pos2 bds)
+    (if (and transient-mark-mode mark-active)
+        (progn
+          (goto-char (region-end))
+          (insert text-end)
+          (goto-char (region-beginning))
+          (insert text-begin))
+      (progn
+        (setq bds (bounds-of-thing-at-point 'symbol))
+        (goto-char (cdr bds))
+        (insert text-end)
+        (goto-char (car bds))
+        (insert text-begin))))) 
 
 (provide 'my-util-library)
